73b71e7b16cb81824b49d02af92d89079bb9ff72
hsa111 <hsahkk10@gmail.com>
2022-03-30 23:06:08 +0800

    add pods's patch shell script

---
+++ pod-patch/patch.sh
@@ -0,0 +1,8 @@
+diff AFSecurityPolicy.m ../Pods/AFNetworking/AFNetworking/AFSecurityPolicy.m 
+cp AFSecurityPolicy.m ../Pods/AFNetworking/AFNetworking/AFSecurityPolicy.m 
+
+diff SMKCertificateValidator.swift ../Pods/SignalMetadataKit/SignalMetadataKit/src/SMKCertificateValidator.swift
+cp SMKCertificateValidator.swift ../Pods/SignalMetadataKit/SignalMetadataKit/src/SMKCertificateValidator.swift
+
+diff SSLSecurity.swift ../Pods/Starscream/Sources/Starscream/SSLSecurity.swift
+cp SSLSecurity.swift ../Pods/Starscream/Sources/Starscream/SSLSecurity.swift
